a17e60748f7d82b6477a08d98fe4815f6bfed3ba
KanYama507 <kanno.yamato9919@gmail.com>
2026-07-06 23:35:07 +0900

    画像追加

---
+++ script.js
@@ -0,0 +1,42 @@
+const images = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        lightbox.style.display = "flex";
+        lightboxImg.src = img.src;
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+    if(e.target === lightbox){
+        lightbox.style.display = "none";
+    }
+});
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        lightbox.style.display = "none";
+    }
+});
+
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        button.classList.toggle("active");
+
+        const panel = button.nextElementSibling;
+
+        panel.classList.toggle("open");
+    });
+
+});
